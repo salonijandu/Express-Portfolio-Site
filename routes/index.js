8c084e896bb7ad5dfa324bf3d564511e5af91a8c
Saloni Saloni <200396451@student.georgianc.on.ca>
2020-06-29 18:12:52 -0400

    Added pages and Navigation between all pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Portfolio' });
 });
+
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'Contact-Me' });
+});
+
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About-Me' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
 
 module.exports = router;
